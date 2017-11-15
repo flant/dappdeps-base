58ba158aa11f7c18c85357f0fe42f38208a712c1
Timofey Kirillov <timofey.kirillov@flant.com>
2017-11-15 19:30:52 +0300

    ++

--- config/software/glibc.rb
+++ config/software/glibc.rb
@@ -14,16 +14,15 @@ build do
   env = with_standard_compiler_flags(with_embedded_path)
 
   command "mkdir build"
-  command "cd build"
 
   command(
     "../configure --prefix=#{install_dir}/embedded" +
     " --disable-werror --enable-kernel=3.2 --enable-stack-protector=strong" +
     " libc_cv_slibdir=#{install_dir}/embedded/lib",
-    env: env
+    env: env,
+    cwd: "build",
   )
 
   command "make -j #{workers}", env: env
   command 'make install', env: env
 end
-
