fb89c11b2d02566d9a95ca33028077822385cfe1
Timofey Kirillov <timofey.kirillov@flant.com>
2017-11-15 19:08:33 +0300

    ++

---
+++ config/software/glibc.rb
@@ -0,0 +1,26 @@
+name "glibc"
+default_version "2.26"
+
+license 'GPL-2'
+license_file 'COPYING'
+
+version("2.26") { source md5: "ae2a3cddba005b34792dabe8c103e866" }
+
+source url: "https://ftp.gnu.org/gnu/glibc/glibc-#{version}.tar.gz"
+
+relative_path "glibc-#{version}"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command(
+    "./configure --prefix=#{install_dir}/embedded" +
+    " --disable-werror --enable-kernel=3.2 --enable-stack-protector=strong" +
+    " libc_cv_slibdir=#{install_dir}/embedded/lib",
+    env: env
+  )
+
+  command "make -j #{workers}", env: env
+  command 'make install', env: env
+end
+
